9beabd10597c0aa2113878cabb66df262c33cb89
DiKang Li <121960425@qq.com>
2018-06-08 09:12:04 +0800

    gulp加入npm命令中

--- README.md
+++ README.md
@@ -1,30 +1,64 @@
-### 使用gulp来架构常规的运营活动页面
+### 使用 gulp 来架构常规的运营活动页面
 
-#### 选择使用gulp的理由
-1. 不想把珍贵的时间浪费在刷新页面上，希望可以将屏幕分成两半，左边用来预览，右边用来改代码
-2. 公司开发服务器经常挂掉，以前的sftp的开发模式不好用
-3. TinyPng图片压缩的官网经常访问不了，希望自己可以做图片压缩
-4. 活动页面的js和css很多都没有压缩，希望自己可以做压缩
+#### 选择使用 gulp 的理由
+
+1.  不想把珍贵的时间浪费在刷新页面上，希望可以将屏幕分成两半，左边用来预览，右边用来改代码
+2.  公司开发服务器经常挂掉，以前的 sftp 的开发模式不好用
+3.  TinyPng 图片压缩的官网经常访问不了，希望自己可以做图片压缩
+4.  活动页面的 js 和 css 很多都没有压缩，希望自己可以做压缩
 
 #### 效果预览
+
 ![image](https://olpkwt43d.qnssl.com/myapp/livereload.gif)
 
 #### 安装使用
-+ 所需环境nodejs，[nodejs下载地址](https://pan.baidu.com/s/1geLgpqz)，安装过程勾选`add path to system`就不用手动添加安装目录到系统环境变量了
 
-+ 克隆或者下载项目
-	这里最好安装cnpm，npm比较慢，不清楚你可以参考[淘宝cnpm](http://npm.taobao.org/)
-	```
-	git clone https://github.com/Andyliwr/gulp-activityPage.git
-	cnpm install -g gulp
-	cnpm install
-	```
-	想要让chrome支持实时刷新，需要一个chrome插件，[点击这儿去下载插件](https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei),在项目运行时，这个chrome插件中间的空心圆点变成了实心就表示项目已经运行成功了。
+- 所需环境 nodejs，[nodejs 下载地址](https://pan.baidu.com/s/1geLgpqz)，安装过程勾选`add path to system`就不用手动添加安装目录到系统环境变量了
 
-	![image02](http://think2011.qiniudn.com/gulp-livereload2.gif)
+- 克隆或者下载项目
+  这里最好安装`cnpm`或者使用`yarn`，npm 比较慢，不清楚你可以参考[淘宝 cnpm](http://npm.taobao.org/)
+  `git clone https://github.com/Andyliwr/gulp-activityPage.git yarn install yarn run dev`
+- 运行项目
+  直接 cmd 到当前项目下输入`gulp serve`，gulp 会自动帮你打开`localhost:8080`，运行项目下的`index.html`,然后你修改文件就会自动刷新了。所有压缩和编译后的文件都放在了`dist`目录下，提测的时候直接把这个目录上传就好了
 
-+ 运行项目
-	直接cmd到当前项目下输入`gulp serve`，gulp会自动帮你打开`localhost:8080`，运行项目下的`index.html`,然后你修改文件就会自动刷新了。所有压缩和编译后的文件都放在了`dist`目录下，提测的时候直接把这个目录上传就好了
+- 另一个命令
+  执行`gulp build`会将原来生成的文件删除并重新生产一遍
 
-+ 另一个命令
-	执行`gulp build`会将原来生成的文件删除并重新生产一遍
+### 配置文件说明
+
+```json
+{
+  "project": "test",
+  "localServer": {
+    "host": "localhost",
+    "port": "3000" // 启动端口
+  },
+  "tinypngApi": "Q0Q_7x6ppzaVY00KSOu1vc5-FVKyN20J", // tinypng官网给的开发秘钥
+   // sftp设置
+  "upload": {
+    "use": false,
+    "protocol": "sftp",
+    "host": "192.168.47.227",
+    "user": "lidikang",
+    "password": "lidikang",
+    "port": "22",
+    "remotePath": "/home/lidikang/test_page/"
+  }
+}
+```
+
+### 活动页面其他利器
+
+- 移动端调试工具-[`eruda`](https://eruda.liriliri.io/)
+
+```
+<script src="https://cdn.bootcss.com/eruda/1.4.4/eruda.min.js"></script>
+<script>eruda.init();</script>
+```
+
+- 雪碧图生成工具[sprite-generator](https://www.toptal.com/developers/css/sprite-generator)
+
+### 更新历史
+
+- 使用`browser-sync`替换`liveload`，这样运行的时候不用再额安装 chrome 自动刷新插件也能实现自动刷新 ---- 2018 年 04 月 22 日
+- 将`gulp`命令加入到`package.json`的`script`字段中，这样运行的时候就不需要全局安装`gulp`了 ---- 2018 年 06 月 08 日
--- gulpfile.js
+++ gulpfile.js
@@ -25,184 +25,223 @@ const watch = require('gulp-watch')
 const config = require('./config.json')
 var reload = browserSync.reload
 
-
-gulp.task('styles', function () {
-	return gulp.src(['src/styles/**/*.scss', '!src/styles/**/_*.scss']) //会编译styles目录下的以scss结尾的scss文件
-		.pipe(debug({
-			title: 'CSS packing:'
-		}))
-		.pipe(sass({
-			style: 'expanded'
-		}))
-		.pipe(autoprefixer('ios 6', 'android 4'))
-		.pipe(cssBeautify({
-			indent: '  ',
-			openbrace: 'end-of-line',
-			autosemicolon: true
-		}))
-		.pipe(gulp.dest('dist/styles/'))
-		.pipe(rename({
-			suffix: '.min'
-		}))
-		.pipe(minifycss())
-		.pipe(gulp.dest('dist/styles/'))
-		.pipe(reload({stream: true}))
+gulp.task('styles', function() {
+  return gulp
+    .src(['src/styles/**/*.scss', '!src/styles/**/_*.scss']) //会编译styles目录下的以scss结尾的scss文件
+    .pipe(
+      debug({
+        title: 'CSS packing:'
+      })
+    )
+    .pipe(
+      sass({
+        style: 'expanded'
+      })
+    )
+    .pipe(autoprefixer('ios 6', 'android 4'))
+    .pipe(
+      cssBeautify({
+        indent: '  ',
+        openbrace: 'end-of-line',
+        autosemicolon: true
+      })
+    )
+    .pipe(gulp.dest('dist/styles/'))
+    .pipe(
+      rename({
+        suffix: '.min'
+      })
+    )
+    .pipe(minifycss())
+    .pipe(gulp.dest('dist/styles/'))
+    .pipe(reload({ stream: true }))
 })
 
 // 只有eslint通过了才经行script打包
-gulp.task('scripts', ['lint'], function () {
-	return gulp.src('src/scripts/**/*.js')
-		.pipe(debug({
-			title: 'JS packing:'
-		}))
-		.pipe(babel({
-			presets: ['es2015'],
-			// plugins: ['transform-runtime']
-		}))
-		.pipe(gulp.dest('dist/scripts/'))
-		.pipe(rename({
-			suffix: '.min'
-		}))
-		.pipe(uglify())
-		.pipe(gulp.dest('dist/scripts/'))
-		.pipe(reload({stream: true}))
+gulp.task('scripts', ['lint'], function() {
+  return gulp
+    .src('src/scripts/**/*.js')
+    .pipe(
+      debug({
+        title: 'JS packing:'
+      })
+    )
+    .pipe(
+      babel({
+        presets: ['env']
+        // plugins: ['transform-runtime']
+      })
+    )
+    .pipe(gulp.dest('dist/scripts/'))
+    .pipe(
+      rename({
+        suffix: '.min'
+      })
+    )
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/scripts/'))
+    .pipe(reload({ stream: true }))
 })
 
-gulp.task('images', function () {
-	return gulp.src('src/images/**')
-		.pipe(gulp.dest('dist/images'))
-		.pipe(reload({stream: true}))
+gulp.task('images', function() {
+  return gulp
+    .src('src/images/**')
+    .pipe(gulp.dest('dist/images'))
+    .pipe(reload({ stream: true }))
 })
 
-gulp.task('fonts', function () {
-	return gulp.src('src/fonts/**')
-		.pipe(gulp.dest('dist/fonts'))
-		.pipe(reload({stream: true}))
+gulp.task('fonts', function() {
+  return gulp
+    .src('src/fonts/**')
+    .pipe(gulp.dest('dist/fonts'))
+    .pipe(reload({ stream: true }))
 })
 
 // 压缩图片 - tinypng
-gulp.task('tinypng', function () {
-	return gulp.src('src/images/**/*.{png,jpg,jpeg}')
-		.pipe(debug({
-			title: 'tinypng:'
-		}))
-		.pipe(tinypng(config.tinypngApi))
-		.pipe(gulp.dest('./dist/images'))
+gulp.task('tinypng', function() {
+  return gulp
+    .src('src/images/**/*.{png,jpg,jpeg}')
+    .pipe(
+      debug({
+        title: 'tinypng:'
+      })
+    )
+    .pipe(tinypng(config.tinypngApi))
+    .pipe(gulp.dest('./dist/images'))
 })
 
-gulp.task('html', function () {
-	return gulp.src('src/*.html')
-		.pipe(debug({
-			title: 'HTML packing:'
-		}))
-		.pipe(gulp.dest('dist/'))
-		.pipe(reload({stream: true}))
+gulp.task('html', function() {
+  return gulp
+    .src('src/*.html')
+    .pipe(
+      debug({
+        title: 'HTML packing:'
+      })
+    )
+    .pipe(gulp.dest('dist/'))
+    .pipe(reload({ stream: true }))
 })
 
 // 清除所有的生成文件
-gulp.task('clean', function () {
-	return gulp.src(['dist'], {
-		read: false
-	}).pipe(clean())
+gulp.task('clean', function() {
+  return gulp
+    .src(['dist'], {
+      read: false
+    })
+    .pipe(clean())
 })
 
 // 静态服务器
 gulp.task('webserver', function() {
-	browserSync.init({
-		server: {
-			baseDir: './dist/'
-		}
-	})
-	gulp.watch('src/styles/**/*.scss', ['styles'])
-	gulp.watch('src/images/**', ['images'])
-	gulp.watch('src/fonts/**', ['fonts'])
-	gulp.watch('src/scripts/**/*.js', ['scripts'])
-	gulp.watch('src/**/*.html', ['html'])
+  browserSync.init({
+    server: {
+      baseDir: './dist/'
+    }
+  })
+  gulp.watch('src/styles/**/*.scss', ['styles'])
+  gulp.watch('src/images/**', ['images'])
+  gulp.watch('src/fonts/**', ['fonts'])
+  gulp.watch('src/scripts/**/*.js', ['scripts'])
+  gulp.watch('src/**/*.html', ['html'])
 })
 
 gulp.task('lint', () => {
-	return gulp.src(['src/scripts/**/*.js', '!node_modules/**', '!dist/**'])
-		.pipe(eslint())
-		.pipe(eslint.format())
-		.pipe(eslint.failAfterError())
+  return gulp
+    .src(['src/scripts/**/*.js', '!node_modules/**', '!dist/**'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
 })
 
 // 打包文件
-gulp.task('zip', function () {
-	function checkTime(i) {
-		if (i < 10) {
-			i = '0' + i
-		}
-		return i
-	}
-	var d = new Date()
-	var year = d.getFullYear()
-	var month = checkTime(d.getMonth() + 1)
-	var day = checkTime(d.getDate())
-	var hour = checkTime(d.getHours())
-	var minute = checkTime(d.getMinutes())
+gulp.task('zip', function() {
+  function checkTime(i) {
+    if (i < 10) {
+      i = '0' + i
+    }
+    return i
+  }
+  var d = new Date()
+  var year = d.getFullYear()
+  var month = checkTime(d.getMonth() + 1)
+  var day = checkTime(d.getDate())
+  var hour = checkTime(d.getHours())
+  var minute = checkTime(d.getMinutes())
 
-	return gulp.src('./dist/**')
-		.pipe(debug({
-			title: '打包成zip:'
-		}))
-		.pipe(zip(config.project + '-' + year + month + day + '-' + hour + minute + '.zip'))
-		.pipe(gulp.dest('./'))
+  return gulp
+    .src('./dist/**')
+    .pipe(
+      debug({
+        title: '打包成zip:'
+      })
+    )
+    .pipe(zip(config.project + '-' + year + month + day + '-' + hour + minute + '.zip'))
+    .pipe(gulp.dest('./'))
 })
 
 // 上传到远程服务器任务，使用foal定义上传任务
-foal.task('upload', function (filePath, cb) {
-	if (config.upload.protocol === 'ftp' || config.upload.protocol === 'Ftp' || config.upload.protocol === 'FTP') {
-		return gulp.src(filePath)
-			.pipe(debug({
-				title: 'ftp上传:'
-			}))
-			.pipe(ftp({
-				host: config.upload.host,
-				user: config.upload.user,
-				pass: config.upload.password,
-				port: config.upload.port,
-				remotePath: config.upload.remotePath
-			}))
-	} else if (config.upload.protocol === 'sftp' || config.upload.protocol === 'Sftp' || config.upload.protocol === 'SFTP') {
-		return gulp.src(filePath)
-			.pipe(debug({
-				title: 'sftp上传:'
-			}))
-			.pipe(sftp({
-				host: config.upload.host,
-				user: config.upload.user,
-				pass: config.upload.password,
-				port: config.upload.port,
-				remotePath: config.upload.remotePath
-			}))
-	}
+foal.task('upload', function(filePath, cb) {
+  if (config.upload.protocol === 'ftp' || config.upload.protocol === 'Ftp' || config.upload.protocol === 'FTP') {
+    return gulp
+      .src(filePath)
+      .pipe(
+        debug({
+          title: 'ftp上传:'
+        })
+      )
+      .pipe(
+        ftp({
+          host: config.upload.host,
+          user: config.upload.user,
+          pass: config.upload.password,
+          port: config.upload.port,
+          remotePath: config.upload.remotePath
+        })
+      )
+  } else if (config.upload.protocol === 'sftp' || config.upload.protocol === 'Sftp' || config.upload.protocol === 'SFTP') {
+    return gulp
+      .src(filePath)
+      .pipe(
+        debug({
+          title: 'sftp上传:'
+        })
+      )
+      .pipe(
+        sftp({
+          host: config.upload.host,
+          user: config.upload.user,
+          pass: config.upload.password,
+          port: config.upload.port,
+          remotePath: config.upload.remotePath
+        })
+      )
+  }
 })
 
 // 默认任务
-gulp.task('default', function () {
-	gulp.start('styles')
-	gulp.start('scripts')
-	gulp.start('images')
-	gulp.start('fonts')
-	gulp.start('html')
-	gulp.start('webserver')
+gulp.task('default', function() {
+  gulp.start('styles')
+  gulp.start('scripts')
+  gulp.start('images')
+  gulp.start('fonts')
+  gulp.start('html')
+  gulp.start('webserver')
 })
 
-gulp.task('build', ['clean'], function () {
-	gulp.start('styles')
-	gulp.start('scripts')
-	gulp.start('images')
-	gulp.start('fonts')
-	gulp.start('html')
+gulp.task('build', ['clean'], function() {
+  gulp.start('styles')
+  gulp.start('scripts')
+  gulp.start('images')
+  gulp.start('fonts')
+  gulp.start('html')
 })
 
 // 提交代码执行的任务
-gulp.task('build2', ['clean'], function () {
-	gulp.start('styles')
-	gulp.start('scripts')
-	gulp.start('tinypng')
-	gulp.start('fonts')
-	gulp.start('html')
-	gulp.start('zip')
+gulp.task('build2', ['clean'], function() {
+  gulp.start('styles')
+  gulp.start('scripts')
+  gulp.start('tinypng')
+  gulp.start('fonts')
+  gulp.start('html')
+  gulp.start('zip')
 })
